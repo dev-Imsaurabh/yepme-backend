a12f08590a0aec9443da285c4f81798dcbadc643
dev-Iamsaurabh <sau4478@gmail.com>
2023-02-24 08:49:45 +0530

    added /admin to get product only of specific admin

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,159 +1,160 @@
-const express = require("express")
-const { adminValidator } = require("../middlewares/adminValidator")
-const { ProductModel } = require("../models/ProductModel")
-const jwt = require("jsonwebtoken")
-const { authenticator } = require("../middlewares/authenticator")
+const express = require("express");
+const { adminValidator } = require("../middlewares/adminValidator");
+const { ProductModel } = require("../models/ProductModel");
+const jwt = require("jsonwebtoken");
+const { authenticator } = require("../middlewares/authenticator");
 
+const productRouter = express.Router();
 
-const productRouter = express.Router()
+productRouter.get("/", async (req, res) => {
+  try {
+    let data = await ProductModel.find(req.query);
+    res.send({
+      message: "All products data",
+      status: 1,
+      data: data,
+      error: false,
+    });
+  } catch (error) {
+    res.send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
 
-
-productRouter.get("/",async(req,res)=>{
-
-    try {
-        let data = await ProductModel.find(req.query)
-        res.send({
-            message:"All products data",
-            status:1,
-            data:data,
-            error:false
-        })
-
-        
-    } catch (error) {
-        res.send({
-            message:"Something went wrong: "+error.message,
-            status:0,
-            error:true
-        })
-        
-    }
-
-})
-
-
-productRouter.get("/:id",async(req,res)=>{
-
-    let {id:_id} = req.params
-    try {
-        let data = await ProductModel.find({_id})
-        res.send({
-            message:"All products data",
-            status:1,
-            data:data,
-            error:false
-        })
-
-        
-    } catch (error) {
-        res.send({
-            message:"Something went wrong: "+error.message,
-            status:0,
-            error:true
-        })
-        
-    }
-
-})
-
+productRouter.get("/:id", async (req, res) => {
+  let { id: _id } = req.params;
+  try {
+    let data = await ProductModel.find({ _id });
+    res.send({
+      message: "All products data",
+      status: 1,
+      data: data,
+      error: false,
+    });
+  } catch (error) {
+    res.send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
 
 //search route here
 
-productRouter.use(authenticator)
+productRouter.use(authenticator);
 
-productRouter.post("/", async(req,res)=>{
-    let token = req.headers.authorization
-    jwt.verify(token, process.env.SecretKey, async function(err, decoded) {
-
-        if(err) res.send({
-            message:"Something went wrong: "+err,
-            status:0,
-            error:true
-        })
-        
-        req.body.forEach(el => {
-            el.adminId="admin"+decoded.userId
-
-        });
-        try {
-           await ProductModel.insertMany(req.body)
-            res.send({
-                message:"Product added",
-                status:1,
-                error:false
-            })
-    
-            
-        } catch (error) {
-            res.send({
-                message:"Something went wrong: "+error.message,
-                status:0,
-                error:true
-            })
-            
-        }
-        
-     
+productRouter.post("/", async (req, res) => {
+  let token = req.headers.authorization;
+  jwt.verify(token, process.env.SecretKey, async function (err, decoded) {
+    if (err)
+      res.send({
+        message: "Something went wrong: " + err,
+        status: 0,
+        error: true,
       });
 
-   
-
-})
-
-
-productRouter.use(adminValidator)
-
-
-productRouter.patch("/:id",async(req,res)=>{
-
-    let {id:_id} = req.params
-
+    req.body.forEach((el) => {
+      el.adminId = "admin" + decoded.userId;
+    });
     try {
-         await ProductModel.findByIdAndUpdate({_id},req.body)
-        res.send({
-            message:"Product updated",
-            status:1,
-            error:false
-        })
-
-        
+      await ProductModel.insertMany(req.body);
+      res.send({
+        message: "Product added",
+        status: 1,
+        error: false,
+      });
     } catch (error) {
-        res.send({
-            message:"Something went wrong: "+error.message,
-            status:0,
-            error:true
-        })
-        
+      res.send({
+        message: "Something went wrong: " + error.message,
+        status: 0,
+        error: true,
+      });
     }
+  });
+});
 
-})
+productRouter.use(adminValidator);
 
+productRouter.patch("/:id", async (req, res) => {
+  let { id: _id } = req.params;
 
+  try {
+    await ProductModel.findByIdAndUpdate({ _id }, req.body);
+    res.send({
+      message: "Product updated",
+      status: 1,
+      error: false,
+    });
+  } catch (error) {
+    res.send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
 
-productRouter.delete("/:id",async(req,res)=>{
+productRouter.delete("/:id", async (req, res) => {
+  let { id: _id } = req.params;
 
-    let {id:_id} = req.params
+  try {
+    await ProductModel.findByIdAndDelete({ _id });
+    res.send({
+      message: "Product deleted",
+      status: 1,
+      error: false,
+    });
+  } catch (error) {
+    res.send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
 
-    try {
-        await ProductModel.findByIdAndDelete({_id})
+productRouter.get("/admin", async (req, res) => {
+  const { token } = req.headers.authorization;
+  jwt.verify(token, process.env.SecretKey, async (err, decoded) => {
+    if (err)
+      res.send({
+        message: "Invalid token: " + err,
+        status: 0,
+        error: true,
+      });
+
+    if (decoded) {
+      try {
+        let data = await ProductModel.find({ _id: "admin" + decoded.userId });
         res.send({
-            message:"Product deleted",
-            status:1,
-            error:false
-        })
-
-        
-    } catch (error) {
+          message: "All products data",
+          status: 1,
+          data: data,
+          error: false,
+        });
+      } catch (error) {
         res.send({
-            message:"Something went wrong: "+error.message,
-            status:0,
-            error:true
-        })
-        
+          message: "Something went wrong: " + error.message,
+          status: 0,
+          error: true,
+        });
+      }
+    }else{
+
+        res.send({
+            message: "Invalid token:",
+            status: 0,
+            error: true,
+          });
+
     }
+  });
+});
 
-})
-
-module.exports={
-    productRouter
-}
+module.exports = {
+  productRouter,
+};
